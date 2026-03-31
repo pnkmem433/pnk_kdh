ff4f152b0bc4fca5d8062f2761de457d2367446e
KDH <pnkmem433@gmail.com>
2026-03-31 18:15:50 +0900

    Add Tasmota smart plug custom build setup

---
+++ tasmota/Tasmota/tasmota/tasmota_xdrv_driver/xdrv_98_smartplug_custom.ino
@@ -0,0 +1,120 @@
+#ifdef USE_SMARTPLUG_CUSTOM
+/*********************************************************************************************\
+ * xdrv_98_smartplug_custom.ino - Smart plug command/status bridge for Tasmota
+\*********************************************************************************************/
+
+#define XDRV_98 98
+
+namespace Spc98 {
+
+const uint32_t kSmartplugStatusPeriodSeconds = 5;
+uint32_t smartplug_seconds_until_publish = kSmartplugStatusPeriodSeconds;
+
+void SpCustomMakeTopic(char* buffer, size_t size, const char* suffix) {
+  snprintf_P(buffer, size, PSTR("smart_plug/%s/%s"), TasmotaGlobal.mqtt_topic, suffix);
+}
+
+bool SpCustomIsOn() {
+  return bitRead(TasmotaGlobal.power, 0);
+}
+
+void SpCustomMakeStatusPayload(char* buffer, size_t size) {
+  snprintf_P(buffer, size, PSTR("{\"state\":\"%s\"}"), GetStateText(SpCustomIsOn()));
+}
+
+void SpCustomPublishStatus() {
+  char topic[TOPSZ];
+  char payload[32];
+  SpCustomMakeTopic(topic, sizeof(topic), "status");
+  SpCustomMakeStatusPayload(payload, sizeof(payload));
+  MqttPublishPayload(topic, payload);
+}
+
+bool SpCustomHandleCommand(const char* cmd) {
+  if (!cmd || !cmd[0]) {
+    return false;
+  }
+
+  if (!strcasecmp(cmd, "on")) {
+    ExecuteCommandPower(1, POWER_ON, SRC_MQTT);
+    SpCustomPublishStatus();
+    return true;
+  }
+
+  if (!strcasecmp(cmd, "off")) {
+    ExecuteCommandPower(1, POWER_OFF, SRC_MQTT);
+    SpCustomPublishStatus();
+    return true;
+  }
+
+  if (!strcasecmp(cmd, "status")) {
+    SpCustomPublishStatus();
+    return true;
+  }
+
+  return false;
+}
+
+void SpCustomSubscribe() {
+  char topic[TOPSZ];
+  SpCustomMakeTopic(topic, sizeof(topic), "command");
+  MqttSubscribe(topic);
+}
+
+bool SpCustomHandleMqttData() {
+  char expected_topic[TOPSZ];
+  SpCustomMakeTopic(expected_topic, sizeof(expected_topic), "command");
+
+  if (strcmp(XdrvMailbox.topic, expected_topic)) {
+    return false;
+  }
+
+  JsonParser parser((char*)XdrvMailbox.data);
+  JsonParserObject root = parser.getRootObject();
+  if (!root) {
+    return true;
+  }
+
+  const char* cmd = root.getStr(PSTR("cmd"), "");
+  SpCustomHandleCommand(cmd);
+  return true;
+}
+
+void SpCustomInit() {
+  smartplug_seconds_until_publish = kSmartplugStatusPeriodSeconds;
+}
+
+void SpCustomEverySecond() {
+  if (smartplug_seconds_until_publish > 0) {
+    smartplug_seconds_until_publish--;
+  }
+
+  if (0 == smartplug_seconds_until_publish) {
+    SpCustomPublishStatus();
+    smartplug_seconds_until_publish = kSmartplugStatusPeriodSeconds;
+  }
+}
+
+}  // namespace Spc98
+
+bool Xdrv98(uint32_t function) {
+  switch (function) {
+    case FUNC_INIT:
+      Spc98::SpCustomInit();
+      break;
+    case FUNC_MQTT_SUBSCRIBE:
+      Spc98::SpCustomSubscribe();
+      break;
+    case FUNC_MQTT_DATA:
+      return Spc98::SpCustomHandleMqttData();
+    case FUNC_SET_DEVICE_POWER:
+      Spc98::SpCustomPublishStatus();
+      break;
+    case FUNC_EVERY_SECOND:
+      Spc98::SpCustomEverySecond();
+      break;
+  }
+  return false;
+}
+
+#endif  // USE_SMARTPLUG_CUSTOM
---
+++ tasmota/Tasmota/tasmota/user_config_override.h
@@ -0,0 +1,28 @@
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+
+#warning **** user_config_override.h: Using Smart Plug Settings ****
+
+#undef STA_SSID1
+#define STA_SSID1         "CC-Retail"
+
+#undef STA_PASS1
+#define STA_PASS1         "pnks1111"
+
+#undef MQTT_HOST
+#define MQTT_HOST         "gym907-0001.iptime.org"
+
+#undef MQTT_PORT
+#define MQTT_PORT         1883
+
+#undef MQTT_USER
+#define MQTT_USER         "pnks"
+
+#undef MQTT_PASS
+#define MQTT_PASS         "pnks1111"
+
+#ifndef USE_SMARTPLUG_CUSTOM
+#define USE_SMARTPLUG_CUSTOM
+#endif
+
+#endif  // _USER_CONFIG_OVERRIDE_H_
